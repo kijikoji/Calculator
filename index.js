5680c45d322b7e01cd081dfd608ab020938fc70c
kijikoji <102621826+kijikoji@users.noreply.github.com>
2024-11-24 18:27:38 -0600

    add operate function

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
 let displayValue = [];
 const displayDiv = document.querySelector(".display");
+let secondDecimal = false
 
 
 const updateDisplay = function() {
     displayDiv.textContent = displayValue.join("");
+    // console.log(displayValue.join(""));
 };
 
 const addDisplay = function(input) {
-    // console.log(input);
+    if(input == ".") {
+        if (secondDecimal) {
+            return;
+        };
+        secondDecimal = true;
+    };
+    if(input == "/" || input == "*" || input == "-" || input == "+") {
+        secondDecimal = false
+    }
     displayValue.push(input);
     updateDisplay();
-}
+};
+
+const clearDisplay = function() {
+    displayValue = [];
+    updateDisplay();
+};
+
+const operate = function() {
+    numberPair = displayValue.join("").split(/(\+|\-|\/|\*)/);
+    console.log(numberPair);
+    let sum = 0;
+    operator = true;
+
+    while(operator) {
+        operator = numberPair.find((value) => {
+            if (value == "*" || value == "/" || value == "+" || value == "-") {
+                return value;
+            };
+        });            
+        switch (operator) {
+            case "*":
+                sum = [Number(numberPair[0]) 
+                    * Number(numberPair[2])];
+                break;
+            case "/":
+                sum = [Number(numberPair[0]) 
+                    / Number(numberPair[2])];
+                break;
+            case "+":
+                sum = [Number(numberPair[0]) 
+                    + Number(numberPair[2])];
+                break;
+            case "-":
+                sum = [Number(numberPair[0]) 
+                    - Number(numberPair[2])];
+                break;
+        };
+        if(operator) {
+            console.log(sum);
+            const arrayEnding = numberPair.slice(3);
+            numberPair = sum.concat(arrayEnding)
+            console.log(numberPair);
+        };
+    };
+    addDisplay(" = " + numberPair);
+};
 
 function main () {
     const inputs = Array.from(document.querySelectorAll(".input"));
@@ -18,6 +73,12 @@ function main () {
     inputs.forEach(input => {
         input.addEventListener("click", () => addDisplay(input.textContent));
     })
+    
+    const equals = document.querySelector(".equals");
+    equals.addEventListener("click", operate)
+
+    const clear = document.querySelector(".clear");
+    clear.addEventListener("click", clearDisplay)
         
     return 0;
 };
